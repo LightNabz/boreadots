20ea17c0d48be7e8d9ec9d9ed10a95c4ce1f786a
LightNabz <nadif.bukhari2110@gmail.com>
2025-03-14 18:03:36 +0800

    First commit

---
+++ symlink.sh
@@ -0,0 +1,15 @@
+ln -s ~/.config/hypr ~/.dotfiles/hypr
+ln -s ~/.config/kitty ~/.dotfiles/kitty
+ln -s ~/.config/nvim ~/.dotfiles/nvim
+ln -s ~/.config/waybar ~/.dotfiles/waybar
+ln -s ~/.config/rofi ~/.dotfiles/rofi
+ln -s ~/.config/sway ~/.dotfiles/sway
+ln -s ~/.config/swaylock ~/.dotfiles/swaylock
+ln -s ~/.config/qt5ct ~/.dotfiles/qt5ct
+ln -s ~/.config/gtk-3.0 ~/.dotfiles/gtk-3.0
+ln -s ~/.config/gtk-4.0 ~/.dotfiles/gtk-4.0
+ln -s ~/.config/htop ~/.dotfiles/htop
+ln -s ~/.config/nwg-look ~/.dotfiles/nwg-look
+ln -s ~/.zshrc ~/.dotfiles/zshrc
+ln -s ~/.bashrc ~/.dotfiles/bashrc
+
